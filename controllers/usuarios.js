b2b0206a0aa6975629d937b31ac48300a80def37
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2020-09-09 19:11:18 -0500

    FIx | Add user controller fix for login function

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -76,12 +76,12 @@ function iniciarSesion(req,res,next) {
   passport.authenticate('local', {session: false}, (err, user, info) => {
     if(err) { return next(err) }
     if(user) {
-      user.token = user.generarJWT();
+      user.token = user.generateJWT();
       return res.json({ user: user.toAuthJSON() })
     } else {
       return res.status(422).json(info)
     }
-  })
+  })(req,res,next)
 }
 
 module.exports = {
